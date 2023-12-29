21f2d16985f9e68a99dacd927c2b884d9bd266ff
unknown <jritsqmet@gmail.com>
2023-12-28 19:04:10 -0500

    actualizacion

--- screens/FormularioScreen.tsx
+++ screens/FormularioScreen.tsx
@@ -1,10 +1,35 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Button, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { TextInput } from 'react-native-gesture-handler'
 
 export default function FormularioScreen() {
+
+  const [numero1, setnumero1] = useState(0);
+  const [numero2, setnumero2] = useState(0);
+  const [respuesta, setrespuesta] = useState(0)
+
+  function sumar( ){
+    setrespuesta( numero1 + numero2 )
+  }
+
+
   return (
     <View>
       <Text>FormularioScreen</Text>
+      <TextInput 
+        placeholder='Ingrese primer numero'
+        keyboardType='numeric'
+        onChangeText={ (texto) => ( setnumero1(+texto) ) }
+      />
+      <TextInput 
+        placeholder='Ingrese segundo numero'
+        keyboardType='numeric'
+        onChangeText={ (texto) => setnumero2(+texto) }
+      />
+
+      <Text>{respuesta}</Text>
+
+      <Button title='sumar' onPress={ () => (sumar()) }/>
     </View>
   )
 }
